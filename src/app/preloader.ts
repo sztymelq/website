f57a87227638bb2087cdd45b2da4a9417e413af0
Sztymelski, Piotr <sztymelskip@ryanair.com>
2018-11-18 13:28:33 +0100

    Initial css setup, preloading all modules enabled

--- src/app/preloader.ts
+++ src/app/preloader.ts
@@ -1,18 +1,10 @@
 import { Injectable } from '@angular/core';
 import { PreloadingStrategy, Route } from '@angular/router';
 import { Observable, of } from 'rxjs';
-import { delay, switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class Preloader implements PreloadingStrategy {
   preload(route: Route, fn: () => Observable<any>): Observable<any> {
-    console.log('route', route);
-    return of('').pipe(
-      delay(3000),
-      switchMap(() => {
-        console.log('preloading!', fn);
-        return fn();
-      })
-    );
+    return fn();
   }
 }
