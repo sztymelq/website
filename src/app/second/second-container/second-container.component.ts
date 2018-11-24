2dbe0c46f7dea3471bc3e842615b21139116ed5b
Piotr Sztymelski <sztymel@gmail.com>
2018-11-24 21:25:41 +0100

    Navigator offline online event handling

--- src/app/second/second-container/second-container.component.ts
+++ src/app/second/second-container/second-container.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent, combineLatest } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-second-container',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second-container.component.scss']
 })
 export class SecondContainerComponent implements OnInit {
+  status = 'online';
+  isAlive: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
+    fromEvent(window, 'offline').pipe(takeWhile(() => this.isAlive)).subscribe(({ type }) => this.setStatus(type));
+    fromEvent(window, 'online').pipe(takeWhile(() => this.isAlive)).subscribe(({ type }) => this.setStatus(type));
+  }
+
+  ngOnDestroy(): void {
+    this.isAlive = false;
+  }
+
+  setStatus(val) {
+    console.log('type', val);
+    this.status = val;
   }
 
 }
